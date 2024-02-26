62d9812d98834a98bc728a6922d5df9f13d4096d
stephliu39 <sliu39@uw.edu>
2024-02-26 15:45:37 -0800

    home page debug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,62 +3,8 @@ import React, {useState} from 'react';
 export default function Home(props) {
     return (
     <body>
-        <header>
-            <div className="overlap">
-                <img src="photos/logo.png" alt="ArmyBase logo of a purple whale" height="120" width="120" />
-            </div>
-        </header>  
-      {/* <nav className="site-navbar">
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-3 col-md-3 col-lg-3 col-xl-3 td">
-          <a href="#">Home</a>
-    
-            </div>
-            <div className="col-md-3 col-lg-3 col-xl-3 td">
-          <a href="calendar.html">Calendar</a>
-        </div>
-        <div className="col-md-3 col-lg-3 col-xl-3 td">
-          <a href="game.html">Guess the Song</a>
-        </div>
-        <div className="col-md-3 col-lg-3 col-xl-3 td">
-          <a href="card-trading.html">Card Trading</a>
-        </div>
-        </div>
-        </div>
-      </nav> */}
-    
-     
-        {/* <nav className="navbar navbar-dark site-navbar" id="mobile">
-         
-     
-          <button className="navbar-toggler ms-auto" type="button" id ="toggle" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon fas fa-bars fa-1x"><i
-              className = "fas fa-bars fa-1x"></i></span>
-        </button>
-        
-        <div className = "collapse navbar-collapse" id = "navbarSupportedContent1">
-           <ul className = "navbar-nav mr-auto">
-              <li className = "nav-item active">
-                <a className = "nav-link" href="#">Home</a>
-              </li>
-              <li className = "nav-item">
-                 <a className = "nav-link" href="card-trading.html">Card Trading</a>
-              </li>
-              <li className = "nav-item">
-                <a className = "nav-link" href="game.html">Guess the Song</a>
-              </li>
-              <li className = "nav-item">
-                <a className = "nav-link" href="calendar.html">Calendar</a>
-              </li>
-              
-           </ul>
-        </div>
-       </nav> */}
-      
       <main>
-        
-          <h1>Home</h1>
+        <h1>Home</h1>
         
         <div className="container mobile-buttons" id="mobile-buttons">
           <div className="d-grid gap-2 col-6 mx-auto d-block d-md-none d-lg-none d-xl-none">
@@ -95,27 +41,7 @@ export default function Home(props) {
               </div>
             </div>
         </section>
-        
       </main>
-     
-        <footer>
-          <p> &copy; 2024 ARMY BASE  All Rights Reserved</p>
-          <p> Created By Yenmy Vo, Stephanie Liu, Natalie Hinds, and Carmen Weatherman</p>
-      </footer>
-     
-    
-    {/* <!-- source code for hamburger menu: https://dev.to/thejourneyville/bootstrap-51-navbar-and-hamburger-toggler-4m9h -->
-    <script>
-      let item = document.getElementById('navbarSupportedContent1');
-      let button = document.getElementById('toggle');
-      button.addEventListener('click', () => {
-         if (item.style.display != "block") {
-            item.style.display = "block";
-         } else {
-            item.style.display = "none";
-         }
-      })
-    </script> */}
     </body>
     )
 }
