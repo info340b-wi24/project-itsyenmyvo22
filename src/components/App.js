12c88cfb35ddb7fdabda9aa5f0c1a9b31fe405de
stephliu39 <sliu39@uw.edu>
2024-02-26 17:09:57 -0800

    added routing for incoming/sent rq pages

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import Calendar from '../pages/Calendar';
 import Cards from '../pages/Cards';
 import { Route, Routes, Switch} from "react-router-dom";
 import Leader from '../pages/Leader';
+import IncomingRequests from '../pages/incoming-requests';
+import SentRequests from '../pages/sent-requests';
+import { Link } from 'react-router-dom';
+
 
 // function Outline(props){
 //     let component;
@@ -78,7 +82,9 @@ function App(props){
                 <Route path = "/Game" element = {<Game />} />
                 <Route path = "/Cards" element = {<Cards />}/>
                  {/* <Route path="/" exact component={<Game />} /> */}
-                <Route path="/Leader" element ={<Leader/>} />
+                <Route path = "/Leader" element = {<Leader />} />
+                <Route path = "/incoming-requests" element= { <IncomingRequests /> } />
+                <Route path = "/sent-requests" element= { <SentRequests /> } />
             </Routes>
 
     
@@ -93,48 +99,45 @@ function App(props){
 }
 function Home(props) {
     return (
-    <body>
-      <main>
+    <div>
         <h1>Home</h1>
-
         <div className="container mobile-buttons" id="mobile-buttons">
-          <div className="d-grid gap-2 col-6 mx-auto d-block d-md-none d-lg-none d-xl-none">
+        <div className="d-grid gap-2 col-6 mx-auto d-block d-md-none d-lg-none d-xl-none">
             <a className="btn btn-light" href="sent-requests.html" role="button">Sent Trade Request</a>
             <a className="btn btn-light" href="incoming-requests.html" role="button">Incoming Trade Request</a>
             <a className="btn btn-light" href="#" role="button">Message Inbox</a>
-          </div>
         </div>
-        
-          <div className="row g-0 mx-0" id="desktop">
+        </div>
+
+        <div className="row g-0 mx-0" id="desktop">
             <div className="col">
-              <div className="container-fluid">
+            <div className="container-fluid">
                 <img src="photos/home/purple-clouds.png" alt="background for sent requests button" />
-              </div>
-              <a className="btn-sent" href="sent-requests.html" id="sent">Sent Trade Request</a>
+            </div>
+            <Link to="/sent-requests" className="btn-sent" id="sent">Sent Trade Request</Link>
             </div>
             <div className="col">
-              <div className="container-fluid">
+            <div className="container-fluid">
                 <img src="photos/home/army-ocean.png" alt="background for incoming requests button" />
-              </div>
-              <a className="btn-incoming" href="incoming-requests.html" id="incoming">Incoming Trade Request</a>
             </div>
-          </div>
-      
+            <Link to="/incoming-requests" className="btn-incoming" id="incoming">Incoming Trade Request</Link>
+            </div>
+        </div>
+
         <section id="my-cards">
-          <h2 className="cardh">My Cards</h2>
+        <h2 className="cardh">My Cards</h2>
             <div className="container">
-              <div className="rectangle">
+            <div className="rectangle">
                 <div className="card">
-                  <div className="card-body">
+                <div className="card-body">
                     <a href="add-card.html" className="card-link">Add card</a>
-                  </div>
                 </div>
-              </div>
+                </div>
+            </div>
             </div>
         </section>
-      </main>
-    </body>
-    )
+    </div> 
+    );
 }
 
 
--- src/pages/incoming-requests.js
+++ src/pages/incoming-requests.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from '../components/App';
 
-export function incomingRequests (props) {
+
+export default function IncomingRequests (props) {
     return (
-    <main id="incoming" class="incoming">
-        <h1 class="rqh1">Incoming Requests</h1>
-        <div class="rectangle">
-            <div id="none-sent" class="none">
+    <div>
+        <main className="incoming">
+        <h1 className="rqh1">Incoming Requests</h1>
+        <div className="rectangle">
+            <div id="none-sent" className="none">
             No incoming requests
             </div>
         </div>
-    </main>
+        </main>
+    </div>
     );
 }
