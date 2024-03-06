caf009ecee1a84ee6d238ac2080ef2af3085cd6d
nhinds2 <hinds.natali04@gmail.com>
2024-03-06 12:49:06 -0800

    firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import './style.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZiZHWhm0wCyN1UJAd2ZCjEkVgAHZ0uKg",
+  authDomain: "armybase-c294a.firebaseapp.com",
+  projectId: "armybase-c294a",
+  storageBucket: "armybase-c294a.appspot.com",
+  messagingSenderId: "732191123489",
+  appId: "1:732191123489:web:61b3dc83c990e8db345b60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,84 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import daysOfMonth2024 from '../data/days.json';
-import { Outlet, useParams, useNavigate, Link } from 'react-router-dom';
-//import { getDatabase, onValue, ref, push as firebasePush } from 'firebase/database';
+import React, { useState } from 'react';
+import daysOfMonth2024 from '../data/days.json'
 
-export function Calendar(props) {
+export default function Calendar(props) {
+    const months = Object.keys(daysOfMonth2024);
+    const [displayMonth, setDisplayMonth] = useState(months[2]);
     return (
         <div className="layout d-flex flex-column flex-md-row">
-            <Outlet />
+            <section className="calendar">
+                <MonthBar displayMonth={displayMonth} setDisplayMonth={setDisplayMonth} months={months} />
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>SUN</th>
+                            <th>MON</th>
+                            <th>TUE</th>
+                            <th>WED</th>
+                            <th>THU</th>
+                            <th>FRI</th>
+                            <th>SAT</th>
+                        </tr>
+                        {<CalendarBody displayMonth={displayMonth} />}
+                    </tbody>
+                </table>
+            </section>
             <Events />
         </div>
-    );
-}
-
-export function CalendarTable(props) {
-    const months = Object.keys(daysOfMonth2024);
-    const navigate = useNavigate();
-    let displayMonth = useParams().currMonth;
-    if (props.defaultMonth) {
-        displayMonth = props.defaultMonth;
-    };
-    // const handlePrevMonth = (event) => {
-    //     if (displayMonth !== 'January') {
-    //         const month = months.indexOf(displayMonth);
-    //         const newMonth = months[month - 1];
-    //         console.log(newMonth);
-    //         navigate('/Calendar/:' + newMonth);
-    //     };
-    // };
-    // const handleNextMonth = (event) => {
-    //     if (displayMonth !== months[11]) {
-    //         const month = months.indexOf(displayMonth);
-    //         const newMonth = months[month + 1];
-    //         navigate('/Calendar/:' + newMonth);
-    //     };
-    // };
-    return (
-        <section className="calendar">
-            <MonthBar displayMonth={displayMonth} months={months} />
-            <table>
-                <tbody>
-                    <tr>
-                        <th>SUN</th>
-                        <th>MON</th>
-                        <th>TUE</th>
-                        <th>WED</th>
-                        <th>THU</th>
-                        <th>FRI</th>
-                        <th>SAT</th>
-                    </tr>
-                    {<CalendarBody displayMonth={displayMonth} daysOfMonth2024={daysOfMonth2024} />}
-                </tbody>
-            </table>
-        </section>
     );
 }
 
 function MonthBar(props) {
     let prevClass = 'cal-prev';
     let nextClass = 'cal-next';
-    let prevMonth = props.displayMonth;
-    let nextMonth = props.displayMonth;
     if (props.displayMonth === 'January') {
         prevClass = 'no-cal-prev';
-        nextMonth = months[month + 1];
     } else if (props.displayMonth === 'December') {
         nextClass = 'no-cal-next';
-        prevMonth = months[month - 1];
-    } else {
-        prevMonth = months[month - 1];
-        nextMonth = months[month + 1];
+    }
+    const handlePrevMonth = (event) => {
+        if (props.displayMonth !== 'January') {
+            const currMonth = props.months.indexOf(props.displayMonth);
+            props.setDisplayMonth(props.months[currMonth - 1]);
+        }
+    }
+    const handleNextMonth = (event) => {
+        if (props.displayMonth !== 'December') {
+            const currMonth = props.months.indexOf(props.displayMonth);
+            props.setDisplayMonth(props.months[currMonth + 1]);
+        }
     }
     return (
         <div className="month">
-            <Link to={'/Calendar/:' + prevMonth}className={prevClass}>
+            <button onClick={handlePrevMonth} className={prevClass}>
                 <img src="photos/calendar/month-arrow.png" alt="back arrow"></img>
-            </Link>
+            </button>
             <h1>{props.displayMonth} 2024</h1>
-            <Link to={'/Calendar/:' + nextMonth} className={nextClass}>
+            <button onClick={handleNextMonth} className={nextClass}>
                 <img src="photos/calendar/month-arrow.png" alt="forward arrow"></img>
-            </Link>
+            </button>
         </div>
     );
 }
@@ -123,27 +101,11 @@ function CalendarRow(props) {
 }
 
 function Events(props) {
-    const [eventList, setEventList] = useState([]);
+    const [eventList, setEventList] = useState([{title:'Event 1', date:'March 8, 2024', time:'6:00 pm PST', type:'Concert'}]);
     const [isVisible, setIsVisible] = useState(false);
     const handleClick = (event) => {
         setIsVisible(!isVisible);
     }
-    // const db = getDatabase();
-    // const eventsRef = ref(db, "events");
-    // useEffect(() => {
-    //     const offFunction = onValue(eventsRef, (snapshot) => {
-    //         const eventObjs = snapshot.val();
-    //         const eventKeys = Object.keys(eventObjs);
-    //         const eventArray = eventKeys.map((keyString) => {
-    //             return eventObjs[keyString];
-    //         });
-    //         setEventList(eventArray);
-    //         function cleanup() {
-    //             offFunction();
-    //         };
-    //         return cleanup;
-    //     });
-    // }, []);
     const allEvents = eventList.map((event) => {
         return (
             <div className="event" key={event.date + event.time}>
@@ -159,73 +121,50 @@ function Events(props) {
             <section className="event-section d-flex flex-column">
                 <h1 className="event-heading">Events</h1>
                 <div className="events">
-                    <button  className="add-event" aria-label='Add event' onClick={handleClick}>
+                    <button  className="add-event" onClick={handleClick}>
                         <img src="photos/calendar/event_plus.png" alt="plus icon"></img>
                     </button>
                     {allEvents}
                 </div>
             </section>
-            {isVisible && <Popup isVisible={isVisible} setIsVisible={setIsVisible} />}
+            {isVisible && <Popup isVisible={isVisible} setIsVisible={setIsVisible} eventList={eventList} setEventList={setEventList} />}
         </div>
     );
 }
 
 function Popup(props) {
-    // const [titleValue, setTitleValue] = useState('');
-    // const [dateValue, setDateValue] = useState('');
-    // const [timeValue, setTimeValue] = useState('');
-    // const [eventTypeValue, setEventTypeValue] = useState('');
-    // const handleTitleChange = (event) => {
-    //     const newTitle = event.target.value;
-    //     setTitleValue(newTitle);
-    // }
-    // const handleDateChange = (event) => {
-    //     const newDate = event.target.value;
-    //     setDateValue(newDate);
-    // }
-    // const handleTimeChange = (event) => {
-    //     const newTime = event.target.value;
-    //     setTimeValue(newTime);
-    // }
-    // const handleEventTypeChange = (event) => {
-    //     const newEventType = event.target.value;
-    //     setEventTypeValue(newEventType);
-    // }
     const handleClose = (event) => {
         props.setIsVisible(!props.isVisible);
     }
     const handleCreate = (event) => {
-        // const db = getDatabase();
-        // const eventsRef = ref(db, "events");
-        // eventsRef.push( {title:titleValue, date:dateValue, time:timeValue, type:eventTypeValue} );
+        const newEventList = [...props.eventList, {title:'Event 2', date:'March 27, 2024', time:'4:00 pm PST', type:'Livestream'}];
+        props.setEventList(newEventList);
         props.setIsVisible(!props.isVisible);
     }
     return (
         <div className="popup py-3" id="popup">
-            <button className="btn-close" aria-label='Close event popup' onClick={handleClose}></button>
-            {/* <form>
+            <button className="btn-close" onClick={handleClose}></button>
+            <form>
                 <div className="popup-title">
                     <label htmlFor="title-input" className="title-label">Event Title:</label>
-                    <input type="text" id="title-input" placeholder="My Event" value={titleValue} onChange={handleTitleChange} className="title-input"></input>
+                    <input type="text" id="title-input" placeholder="My Event" className="title-input"></input>
                 </div>
                 <div className="popup-date">
                     <label htmlFor="date-input" className="date-label">Date:</label>
-                    <input type="date" id="date-input" value={dateValue} onChange={handleDateChange} className="date-input"></input>
+                    <input type="date" id="date-input" className="date-input"></input>
                 </div>
                 <div className="popup-time">
                     <label htmlFor="time-input" className="time-label">Time:</label>
-                    <input type="time" id="time-input" value={timeValue} onChange={handleTimeChange} className="time-input"></input>
+                    <input type="time" id="time-input" className="time-input"></input>
                 </div>
-                <div className='popup-event-type'>
-                    <label htmlFor='event-type-select' className='event-type-label'></label>
-                    <select className="popup-select" id='event-type-select' value={eventTypeValue} onChange={handleEventTypeChange}>
-                        <option defaultValue>Livestream</option>
-                        <option value="1">Concert</option>
-                        <option value="2">Meet and Greet</option>
-                    </select>
-                </div>
-                <button className="create-event" aria-label='Save event' onSubmit={handleCreate}>Create Event</button>
-            </form> */}
+                <select className="popup-select">
+                    <option defaultValue>Type of Event</option>
+                    <option value="1">Concert</option>
+                    <option value="2">Livestream</option>
+                    <option value="3">Meet and Greet</option>
+                </select>
+                <button className="create-event" onSubmit={handleCreate}>Create Event</button>
+            </form>
         </div>
     );
 }
