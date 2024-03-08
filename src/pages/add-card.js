a0f46a019a0f5089c560ef546e81793609ca5133
nhinds2 <hinds.natali04@gmail.com>
2024-03-07 17:51:23 -0800

    added user dropdown

--- src/pages/add-card.js
+++ src/pages/add-card.js
@@ -2,6 +2,29 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 //import firebase from 'firebase/compat/app';
 // import {getStorage} from 'firebase/storage';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
+function Drop() {
+    return (
+      <Dropdown >
+        <Dropdown.Toggle className = "bg-dark" variant="success" id="dropdown-basic">
+          Select Member Name
+        </Dropdown.Toggle>
+  
+        <Dropdown.Menu>
+          <Dropdown.Item>BTS</Dropdown.Item>
+          <Dropdown.Item >RM</Dropdown.Item>
+          <Dropdown.Item >JIN</Dropdown.Item>
+          <Dropdown.Item >SUGA</Dropdown.Item>
+          <Dropdown.Item >J-HOPE</Dropdown.Item>
+          <Dropdown.Item >JIMIN</Dropdown.Item>
+          <Dropdown.Item >V</Dropdown.Item>
+          <Dropdown.Item >Jungkook</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
 
 export default function AddCard (props) {
     const [file, setFile] = useState(null);
@@ -9,6 +32,8 @@ export default function AddCard (props) {
     const [uploadProgress, setUploadProgress] = useState(0);
     const [uploadError, setUploadError] = useState(null);
   
+    
+    
     // const storage = getStorage();
 
     //const fileRef = storageRef(storage, "path/to/myfile.png");
@@ -44,16 +69,19 @@ export default function AddCard (props) {
     //   }
     // };
 
-    // return (
-    //     <div className="add-card">
-    //             <p className="request-title">Upload Image</p>
-    //             <Link  aria-label='exit button' className="exit-btn" to='/'>X</Link>
-    //             <div className="inner-add-card">
-    //                 <img className="upload-icon" src="photos/home/upload-icon.png" alt="cloud with an arrow pointing upwards" />
-    //                 {/* <input type="file" className="member-btn trade-btn" multipleonChange={handleFile}>Select File</input> */}
-    //                 <input type="file" onChange={handleFileChange} />
-    //                 <button aria-label='upload button' className="member-btn trade-btn" onClick={handleUpload} disabled={!file || uploading}>Upload</button>
-    //             </div>
-    //     </div>
-    // )  
+    return (
+        <div className="add-card">
+                <p className="request-title">Upload Image</p>
+                <Link  aria-label='exit button' className="exit-btn" to='/'>X</Link>
+                <div className="inner-add-card">
+                    <img className="upload-icon" src="photos/home/upload-icon.png" alt="cloud with an arrow pointing upwards" />
+                    {/* <input type="file" className="member-btn trade-btn" multipleonChange={handleFile}>Select File</input> */}
+                    <input type="file" />
+
+ 
+  <Drop />
+                    <button aria-label='upload button' className="member-btn trade-btn" disabled={!file || uploading}>Upload</button>
+                </div>
+        </div>
+    )  
 }
