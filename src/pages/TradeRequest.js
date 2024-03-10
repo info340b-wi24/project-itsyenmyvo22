4cc803a76ad7465e850304393d705b8f92e5db73
stephliu39 <sliu39@uw.edu>
2024-03-10 14:47:10 -0700

    delete mycards.js

--- src/pages/TradeRequest.js
+++ src/pages/TradeRequest.js
@@ -42,7 +42,7 @@ export default function TradeRequest(props) {
     <div className="trade-request">
         <p className="request-title">Request Trade</p>
         <p>Are you sure you want to send a request to trade this card?</p>
-        <Link aria-label="accept" to='/Accept' className="member-btn" onClick={sendRequest}>Accept</Link>
+        <Link aria-label="accept" to='/Accept' className="member-btn">Accept</Link>
         <Link aria-label="decline" to='/Cards' className="member-btn">Decline</Link>
     </div>
     )
